560576a81408f2606159bdb45db38b8a3a620140
EASY VISA <daniel.ryota123@gmail.com>
2025-08-08 14:39:05 +0100

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-lg">
+      <h1 className="text-2xl font-bold">Hello Tailwind 🚀</h1>
+      <p>Si ce bloc est bleu avec texte blanc, Tailwind fonctionne !</p>
+    </div>
+  );
+}
